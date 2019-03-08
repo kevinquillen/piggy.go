46785f8fa34fb79db50d37fd3dc86b63debe3eff
Kevin Quillen <kevinquillen@gmail.com>
2019-03-07 22:41:00 -0500

    Initial commit.

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	const (
+		nickel  = 0.05
+		dime    = 0.10
+		quarter = 0.25
+	)
+
+	var coins = [3]float64{nickel, dime, quarter}
+	var balance float64
+
+	for balance <= 20 {
+		coin := coins[rand.Intn(3)]
+		balance += coin
+
+		fmt.Printf("Balance is now $%4.2f\n", math.Abs(balance))
+	}
+}
